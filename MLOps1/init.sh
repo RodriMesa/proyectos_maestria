97a5cb8cdec7876f081f5aae83a16946d91b898d
Rodrigo Mesa <rodrigomesamarchi@gmail.com>
2025-07-28 20:22:20 -0300

    Correccion desincronizacion git

--- MLOps1/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./airflow/dags ./airflow/logs ./airflow/plugins
-
-# Asegurar que cualquier usuario pueda usarlo (ideal en entornos colaborativos)
-chmod -R 775 ./airflow
-chown -R 50000:50000 ./airflow
